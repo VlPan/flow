2f6ccfd2e5debd20811a4d5256dac5effcd35ec4
Uladzimir Pantsiukhou <upantsiukhou@slb.com>
2026-04-05 02:02:36 +0200

    Updates

--- chunk-KDX2F5XE.js
+++
@@ -1,1 +0,0 @@
-import{ya as i}from"./chunk-B2KD4SEN.js";import{ba as o,nb as t}from"./chunk-YQKOKR5B.js";var f=(()=>{class e{static \u0275fac=function(r){return new(r||e)};static \u0275mod=t({type:e});static \u0275inj=o({imports:[i]})}return e})();export{f as a};
